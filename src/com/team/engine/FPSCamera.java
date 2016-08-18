eb19456c4f8ffd64b67432045e62e96450776a3d
root <root@JOSEPH-PC>
2016-08-17 22:11:47 -0500

    Made FPSCamera behave the same across different frame rates

--- src/com/team/engine/FPSCamera.java
+++ src/com/team/engine/FPSCamera.java
@@ -18,8 +18,8 @@ public class FPSCamera extends Camera {
 	public float roll = 0;
 	public Vec3 up = new Vec3(0.0f, 1.0f, 0.0f);
 	
-	private static float lastX = 400;
-	private static float lastY = 300;
+	private static final float MOUSE_SENSITIVITY = 50f;
+	private static final float WASD_SENSITIVITY = 5f;
 	
 	/**
 	 * Called by the game loop every frame. When we make GameObjects they'll all have this functionality for multiple objects. For now it's called directly
@@ -30,9 +30,10 @@ public class FPSCamera extends Camera {
 	public void update() {
 		Vec3 right = front.cross(up);
 		float delta = Engine.instance.deltaTime;
+		float sensitivity = MOUSE_SENSITIVITY * delta;
+		float cameraSpeed = WASD_SENSITIVITY * delta;
 		
 		if (Mouse.isButtonDown(0)) {
-			float sensitivity = 0.3f;
 			
 			Mouse.setGrabbed(true);
 			float mouseSpeed = 0.1f;
@@ -64,20 +65,20 @@ public class FPSCamera extends Camera {
 		
 		
 		if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-			Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, 0.3f));
+			Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, sensitivity * 1.5f));
 			Vec4 vec = mat.multiply(new Vec4(up.x, up.y, up.z, 1.0f));
 			up = new Vec3(vec.x, vec.y, vec.z);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-			Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, -0.3f));
+			Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, -sensitivity * 1.5f));
 			Vec4 vec = mat.multiply(new Vec4(up.x, up.y, up.z, 1.0f));
 			up = new Vec3(vec.x, vec.y, vec.z);
 		}
 		
-		float cameraSpeed = 0.1f;
-		
 		if (Controllers.getControllerCount() > 0) {
 			Controller controller = Controllers.getController(0);
+			
+			System.out.println(controller.getAxisValue(0));
 			
 			float moveX = controller.getAxisValue(1);
 			float moveY = controller.getAxisValue(2);
@@ -89,14 +90,14 @@ public class FPSCamera extends Camera {
 			float trigger2 = (2 + (controller.getAxisValue(6) - 1f)) / 2;
 			
 			if (moveX > 0.3f || moveX < -0.3f) {
-				position = position.add(right.multiply(moveX * 0.1f));
+				position = position.add(right.multiply(moveX * cameraSpeed));
 			}
 			if (moveY > 0.3f || moveY < -0.3f) {
-				position = position.add(front.multiply(-moveY * 0.1f));
+				position = position.add(front.multiply(-moveY * cameraSpeed));
 			}
 			if (lookY > 0.3f || lookY < -0.3f) {
 				//vertical
-				Mat4 mat = new Mat4().rotate(new Vec4(right.x, right.y, right.z, -lookY * 0.3f));
+				Mat4 mat = new Mat4().rotate(new Vec4(right.x, right.y, right.z, -lookY * sensitivity * 2));
 				Vec4 vec = mat.multiply(new Vec4(front.x, front.y, front.z, 1.0f));
 				front = new Vec3(vec.x, vec.y, vec.z);
 				
@@ -104,7 +105,7 @@ public class FPSCamera extends Camera {
 				up = new Vec3(vec2.x, vec2.y, vec2.z);
 			}
 			if (lookX > 0.3f || lookX < -0.3f) {
-				Mat4 mat2 = new Mat4().rotate(new Vec4(up.x, up.y, up.z, -lookX * 0.3f));
+				Mat4 mat2 = new Mat4().rotate(new Vec4(up.x, up.y, up.z, -lookX * sensitivity * 2));
 				Vec4 vec3 = mat2.multiply(new Vec4(front.x, front.y, front.z, 1.0f));
 				front = new Vec3(vec3.x, vec3.y, vec3.z);
 			}
@@ -118,12 +119,12 @@ public class FPSCamera extends Camera {
 			}
 			
 			if (trigger1 > 0f) {
-				Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, -trigger1 * 0.5f));
+				Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, -trigger1 * sensitivity * 1.5f));
 				Vec4 vec = mat.multiply(new Vec4(up.x, up.y, up.z, 1.0f));
 				up = new Vec3(vec.x, vec.y, vec.z);
 			}
 			if (trigger2 > 0f) {
-				Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, trigger2 * 0.5f));
+				Mat4 mat = new Mat4().rotate(new Vec4(front.x, front.y, front.z, trigger2 * sensitivity * 1.5f));
 				Vec4 vec = mat.multiply(new Vec4(up.x, up.y, up.z, 1.0f));
 				up = new Vec3(vec.x, vec.y, vec.z);
 			}
