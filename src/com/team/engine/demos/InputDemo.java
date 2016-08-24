9ec42773620ab77fec9cad10dc0f27a5b55eb4fd
Kalman98 <kalman98k@gmail.com>
2016-08-23 22:07:00 -0500

    Added the RoboKalServer code.

--- src/com/team/engine/demos/InputDemo.java
+++ src/com/team/engine/demos/InputDemo.java
@@ -1,5 +1,12 @@
 package com.team.engine.demos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import org.lwjgl.input.Controller;
 import org.lwjgl.input.Controllers;
 
@@ -7,7 +14,21 @@ import com.team.engine.Engine;
 import com.team.engine.Shader;
 
 public class InputDemo extends Engine {
+	
+	static Socket socket;
+	static PrintWriter out;
+	static BufferedReader in;
+	static int port;
+	
 	public static void main(String[] args) {
+		
+		if (args.length != 1) {
+			port = 5660;
+		} else if (args[0].toString() == "help") {
+			System.out.println("Usage: java RoboKalServer <port number>");
+			System.exit(1);
+		} else port = Integer.parseInt(args[0]);
+		
 		new InputDemo().initialize(false);
 	}
 	
@@ -22,11 +43,27 @@ public class InputDemo extends Engine {
 	
 	@Override
 	public void tick() {
-		float lookX = controller.getAxisValue(4);
-		float lookY = controller.getAxisValue(5);
+		try (ServerSocket serverSocket = new ServerSocket(port)) { 
+				socket = serverSocket.accept();
+		} catch (IOException e) {
+			System.err.println("Could not listen on port " + port + ". Is it open?");
+			System.exit(-1);
+		}
 		
-		if (lookX > 0.3f || lookX < -0.3f || lookY > 0.3f || lookY < -0.3f) {
-			System.out.println("send packet: ("+lookX+","+lookY+")");
+		try {
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(
+					new InputStreamReader(
+							socket.getInputStream()));
+		} catch (Exception e) {}
+	
+		while (true) {
+			float lookX = controller.getAxisValue(4);
+			float lookY = controller.getAxisValue(5);
+			
+			if (lookX > 0.3f || lookX < -0.3f || lookY > 0.3f || lookY < -0.3f) {
+				out.print("+lookX+" + "+lookY+");
+			}
 		}
 	}
 
