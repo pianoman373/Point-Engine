913600152c457859af69e3c11950840ae6d86150
pianoman373 <jholley373@yahoo.com>
2016-10-24 03:33:08 -0500

    Stuff

--- src/com/team/engine/Settings.java
+++ src/com/team/engine/Settings.java
@@ -1,7 +1,7 @@
 package com.team.engine;
 
 public class Settings {
-	public static final boolean ENABLE_SHADOWS = true;
+	public static final boolean ENABLE_SHADOWS = false;
 	public static final int SHADOW_RESOLUTION = 1024;
 	public static final boolean ENABLE_NORMAL_MAPPING = true;
 	public static final boolean ENABLE_BLOOM = false;
@@ -9,7 +9,7 @@ public class Settings {
 	public static int WINDOW_WIDTH = 1344;
 	public static int WINDOW_HEIGHT = 1512;
 	public static final boolean FULLSCREEN = false;
-	public static final float MAX_FPS = 400;
+	public static final float MAX_FPS = 50;
 	/**
 	 * Where all resources start at.
 	 */
